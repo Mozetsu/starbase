b87b0ace949ef1cec00e191f0f38ae2962f3aa04
Mozetsu <code.moz.00@gmail.com>
2022-01-09 20:14:05 +0000

    Refactor navbar, and hero components.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const burguerBtn = document.querySelector('.btn__burguer');
 const navbarItems = document.querySelector('.navbar__items');
+const htmlElement = document.querySelector('body');
 
 burguerBtn.addEventListener('click', function () {
 	navbarItems.classList.toggle('items--expanded');
+	htmlElement.classList.toggle('no-scroll');
 });
